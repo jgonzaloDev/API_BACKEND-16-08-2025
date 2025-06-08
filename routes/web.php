a8e71727f1c2efa1240348404707d86917a0bd7b
Your Name <you@example.com>
2025-06-08 11:17:29 -0500

    chore: crud for laravel and log manager

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\AlumnoController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [AlumnoController::class, 'index']);
+
+// Route::resource('alumnos', AlumnoController::class);
+Route::get('alumnos',[AlumnoController::class,'index']);
+Route::get('alumnos/{id}',[AlumnoController::class,'show']);
+Route::post('alumnos',[AlumnoController::class,'store']);
+Route::put('alumnos/{id}',[AlumnoController::class,'update']);
+Route::delete('alumnos/{id}',[AlumnoController::class,'delete']);
+
+
