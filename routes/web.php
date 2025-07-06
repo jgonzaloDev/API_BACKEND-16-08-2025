9e10d2920f8755e6a0df047cc876d77a8fc5e7f2
Christian Ramirez <christian.devsw@gmail.com>
2025-07-05 20:52:02 -0500

    Actualizacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/blobs', [AzureBlobController::class, 'listarBlobs']);
 Route::get('/blob/{name}', [AzureBlobController::class, 'obtenerBlob']);
 
-
-
 Route::get('/', [AlumnoController::class, 'index']);
 
 Route::get('alumnos',[AlumnoController::class,'index']);
